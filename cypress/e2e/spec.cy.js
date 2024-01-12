d883008029d18e1a5e6ae8219ec4f991460862db
thelifeofayo <ajibadeayobami41@gmail.com>
2024-01-12 11:48:41 +0100

    updated message

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="Cypress" />
+describe('My first suite', () => {
+  it('my first test case ', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
   })
 })
