a0419cbfd7e587090544cffcfd3acc9619080ee1
thelifeofayo <ajibadeayobami41@gmail.com>
2024-01-15 15:11:35 +0100

    Get and find command

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,7 @@ describe('My first suite', () => {
   it('my first test case ', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     cy.get('.search-keyword').type('ca')
+    cy.get('.product:visible').should('have.length',4)
+    cy.get('.products').find('.product').eq(0).contains('ADD TO CART').click()
   })
 })
